d52ef7e4750745cd0bb64541633487af03fb59f4
Ajay Basra <ajay_b@hotmail.co.nz>
2023-04-24 15:14:40 +1200

    test & refactor: argumentparser tests refactored

--- GameOfLifeTests/ArgumentParserTests.cs
+++ GameOfLifeTests/ArgumentParserTests.cs
@@ -1,15 +1,35 @@
 using GameOfLife;
+using GameOfLife.Interfaces;
+using Moq;
 
 namespace GameOfLifeTests;
 
 public class ArgumentParserTests
 {
+    private readonly ArgumentParser _argumentParser;
+    private readonly Mock<ICommandLine> _mockedCommandLine;
+    private readonly string[] expectedArgs = { "pathname", "2", "5", "5" };
+    public ArgumentParserTests()
+    {
+        _mockedCommandLine = new Mock<ICommandLine>();
+        _mockedCommandLine.Setup(x => x.GetCommandLineArgs()).Returns(expectedArgs);
+        _argumentParser = new ArgumentParser(_mockedCommandLine.Object);
+    }
+
+    [Fact]
+    public void GetCommandLineArgs_ReturnsRightOutput_WhenCalled()
+    {
+        var actual = _mockedCommandLine.Object.GetCommandLineArgs();
+        
+        Assert.Equal(expectedArgs, actual);
+    }
+    
     [Fact]
     public void GetChosenGameVersion_ReturnsCorrectValue_WhenCalled()
     {
         var expected = 2;
 
-        var actual = FakeArgumentParser.GetChosenGameVersion();
+        var actual = _argumentParser.GetChosenGameVersion();
         
         Assert.Equal(expected, actual);
     }
@@ -19,7 +39,7 @@ public class ArgumentParserTests
     {
         var expected = new List<int> { 5, 5 };
 
-        var actual = FakeArgumentParser.GetWorldDimensions();
+        var actual = _argumentParser.GetWorldDimensions();
         
         Assert.Equal(expected, actual);
     }
